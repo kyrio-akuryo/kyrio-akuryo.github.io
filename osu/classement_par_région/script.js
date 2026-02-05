7b79070b6e2a5b69d73630b08b41b1b55aa4efb1
Kyrio <kyrio.akuryo@gmail.com>
2026-02-05 20:53:40 +0100

    Update website

--- osu/classement_par_région/script.js
+++ osu/classement_par_région/script.js
@@ -100,6 +100,15 @@ function renderTable() {
             const groupClass = `group-${groupRaw.toLowerCase()}`;
             let groupBadgeHTML = groupRaw.toLowerCase() !== "default" ? `<span class="group-tag ${groupClass}">${groupRaw}</span>` : "";
             let teamHTML = "-";
+            let supporterHTML = "";
+
+            if (player.has_supported) {
+                supporterHTML = `
+                    <svg class="supporter-icon" viewBox="0 0 24 24">
+                        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+                    </svg>
+                `;
+            }
 
             if (player.team) {
                 if (player.team.flag_url) {
@@ -141,6 +150,7 @@ function renderTable() {
                                 <a href="https://osu.ppy.sh/users/${player.id}" target="_blank" class="player-link ${groupClass}">
                                     ${player.username}
                                 </a>
+                                ${supporterHTML}
                                 ${tooltipHTML}
                             </div>
                             ${groupBadgeHTML}
